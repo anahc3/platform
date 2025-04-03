c729237fb975803f57d45531ccc1dd7a05d0087b
Ana Helena <anahc3@al.insper.edu.br>
2025-04-03 13:54:33 -0300

    errinho

--- api/product-service/src/main/resources/db/migration/V2025.02.21.002__create_table_product.sql
+++ api/product-service/src/main/resources/db/migration/V2025.02.21.002__create_table_product.sql
@@ -1,7 +1,7 @@
 CREATE TABLE product (
-    id_prodcut VARCHAR(36) NOT NULL,
+    id_product VARCHAR(36) NOT NULL,
     tx_name VARCHAR(256) NOT NULL,
     tx_unit VARCHAR(256) NOT NULL,
     double_price DECIMAL(10,2) NOT NULL,
-    CONSTRAINT pk_account PRIMARY KEY (id_prodcut)
+    CONSTRAINT pk_account PRIMARY KEY (id_product)
 );
