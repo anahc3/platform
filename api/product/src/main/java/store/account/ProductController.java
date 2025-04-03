37c5e0fc57db29fc1385136e524974ed8e5c8309
Ana Helena <anahc3@al.insper.edu.br>
2025-04-03 12:54:47 -0300

    product feita

---
+++ api/product/src/main/java/store/account/ProductController.java
@@ -0,0 +1,33 @@
+package store.product;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "product", url = "http://product:8080")
+public interface ProductController {
+
+    @PostMapping("/product")
+    public ResponseEntity<ProductOut> create(
+        @RequestBody ProductIn productIn
+    );
+
+    @GetMapping("/product")
+    public ResponseEntity<List<ProductOut>> findAll();
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<ProductOut> findById(
+        @PathVariable String id
+    );
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<Void> delete(
+        @PathVariable String id
+    );
+
+}
